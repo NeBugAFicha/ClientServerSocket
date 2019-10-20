c5b3cd0fcb49ecf5e2dbadc34a287991cdc3ac54
egor <49631827+GGBET322@users.noreply.github.com>
2019-10-20 22:32:58 +0300

    add ClientServerSocket/

---
+++ ClientServerSocket/Client/src/Client.java
@@ -0,0 +1,19 @@
+import aleksanov.Phone;
+import java.net.*;
+import java.io.*;
+public class Client {
+    public static void main(String[] args) {
+        try(Phone phone = new Phone("127.0.0.1",8000)){
+            System.out.println("Connected to server");
+            String request = "Lipetsk";
+            System.out.println("Request: " + request);
+            phone.writeLine(request);
+            String response = phone.readLine();
+            System.out.println("Response: " + response);
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+
+}
